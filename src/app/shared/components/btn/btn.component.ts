df655abbaf06ac5d039b167fa382a4fcd18d416f
Artur Henrique <artur.hique@gmail.com>
2025-02-21 14:46:48 -0300

    feat: added click event

--- src/app/shared/components/btn/btn.component.ts
+++ src/app/shared/components/btn/btn.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-btn',
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './btn.component.scss'
 })
 export class BtnComponent {
+  @Output() btnClick = new EventEmitter<void>();
 
+  onClick() {
+    this.btnClick.emit();
+  }
 }
