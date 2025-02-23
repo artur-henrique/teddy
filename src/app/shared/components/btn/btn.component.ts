1a2a15899e22633661e34409eee27021753f445a
Artur Henrique <artur.hique@gmail.com>
2025-02-23 09:51:36 -0300

    feat: add btn component with initial styles

--- src/app/shared/components/btn/btn.component.ts
+++ src/app/shared/components/btn/btn.component.ts
@@ -1,12 +1,13 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-btn',
   imports: [],
   templateUrl: './btn.component.html',
-  styleUrl: './btn.component.scss'
+  styleUrl: './btn.component.scss',
 })
 export class BtnComponent {
+  @Input() customClass: string = '';
   @Output() btnClick = new EventEmitter<void>();
 
   onClick() {
