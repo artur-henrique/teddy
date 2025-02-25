e5c3a54cd4a58906cbd524b99c297b9c89998903
Artur Henrique <artur.hique@gmail.com>
2025-02-25 17:27:10 -0300

    refactor: changed api path

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  api: 'api/users',
+  // api: 'api/users',
+  api: 'https://boasorte.teddybackoffice.com.br/users',
   STORAGE_KEY: 'selectedIds',
 };
